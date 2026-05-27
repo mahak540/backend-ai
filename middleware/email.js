aebfdcc2e3d3aa8a6f407e4fcfcfaa18ff67a183
mahak540 <162689029+mahak540@users.noreply.github.com>
2026-05-27 19:40:59 +0530

    Change SMTP port and clean up transporter settings
    Updated SMTP port from 2525 to 587 and removed unnecessary TLS settings.

--- middleware/email.js
+++ middleware/email.js
@@ -8,24 +8,13 @@ const generateOTP = ()=>{
 
 const transporter = nodemailer.createTransport({
     host:"smtp-relay.brevo.com",
-    port:2525
+    port:587
     secure:false,
     auth:{
         user:process.env.BREVO_EMAIL,
         pass:process.env.BREVO_SMPT_KEY,
     }
-    tls:{
-        rejectUnauthorized:false;
-    },
-    connectionTimeout:10000,
-    greetingTimeout:10000,
-    socketTimeout:10000
 });
-transporter.verify().then(()=>
-    console.log("smtp ready"))
-.catch((err)=>
-    console.log("smtp failed"),err));
-    
     
 const sendEmail = async(email,otp)=>{
     try{
