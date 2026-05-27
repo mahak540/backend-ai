a8e5c990abbbfa19b900c0d66b0bceccb9156fd1
mahak540 <162689029+mahak540@users.noreply.github.com>
2026-05-27 19:42:05 +0530

    Update email.js

--- middleware/email.js
+++ middleware/email.js
@@ -8,7 +8,7 @@ const generateOTP = ()=>{
 
 const transporter = nodemailer.createTransport({
     host:"smtp-relay.brevo.com",
-    port:587
+    port:587,
     secure:false,
     auth:{
         user:process.env.BREVO_EMAIL,
