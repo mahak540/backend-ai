40ad03bd8aa3f9f894507a8321a5eeb4e039c45f
mahak540 <162689029+mahak540@users.noreply.github.com>
2026-06-04 15:45:29 +0530

    Update image.js

--- routes/image.js
+++ routes/image.js
@@ -27,15 +27,11 @@ router.post("/generate-image", async(req,res)=>{
             }
         );
 
-        const img =
-  response.data.image ||
-  response.data.imageUrl ||
-  response.data.url ||
-  response.data[0]?.url;
+       
 
-res.status(200).json({
+ res.status(200).json({
   success: true,
-  image: img
+  image: response.data.image;
 });
 
     }catch(err){
@@ -57,19 +53,11 @@ router.post("/generate-video", async (req, res) => {
     const response = await axios.post(`${url}/video`, {
       prompt
     });
-const vid =
-      response.data.video ||
-      response.data.videoUrl ||
-      response.data.url ||
-      response.data[0]?.url;
 
-    return res.status(200).json({
-      success: true,
-      video: vid
+
+    return res.json({
+      video: response.data.video
     });
-    // return res.json({
-    //   video: response.data.video
-    // });
 
   } catch (err) {
     console.log(err);
